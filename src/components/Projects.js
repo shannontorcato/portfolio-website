ad8eabc30c41a93a991cf575f0ff29b0cc39da50
shannontorcato <shannon.torcato1234@gmail.com>
2022-10-25 18:40:54 -0400

    updated projects.js, added ProjectCard.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,27 +40,33 @@ export const Projects = () =>{
                 <Col>
                     <h2>Projects</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, molestias sint odio accusamus fugit quia minima ullam nihil maxime mollitia fuga harum. Sequi perferendis expedita consequatur magni provident! Hic, vitae.</p>
-                    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                    <Row>
-                        <Col sm={3}>
-                        <Nav variant="pills" className="flex-column">
-                            <Nav.Item>
-                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        </Col>
-                        <Col sm={9}>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">
-                            </Tab.Pane>
-                        </Tab.Content>
-                        </Col>
-                    </Row>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link eventKey="first">Tab one</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Tab two</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third">Tab three</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {
+                                    projects.map((project, index) =>{
+                                        return (
+                                            <p>{project.title}</p>
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                        <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+                    </Tab.Content>
                     </Tab.Container>
                 </Col>
             </Row>
